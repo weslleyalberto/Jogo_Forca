d62799ed933deccdc210170c2298e1d528c4dc43
Weslley Alberto <weslleyalberto@gmail.com>
2022-02-04 00:09:35 -0300

    Commit inicial

---
+++ forca.js
@@ -0,0 +1,87 @@
+
+
+
+
+let novaPalavra = document.querySelector("#input-nova-palavra");
+let palavraEscrita = document.querySelector("#palavra-text");
+let buttonAdicionarPalavra = document.querySelector("#nova-palavra");
+let btnPalavra = document.querySelector("#btnPalavra");
+let btnSortearPalavra = document.querySelector("#btn-sortear");
+let span = document.querySelector('span');
+let palavraSecreta = [];
+let btnLimparLista = document.querySelector("#btn-limpar-lista");
+
+btnSortearPalavra.addEventListener("click", function(){
+ 
+  
+  return sortearPalavra();
+});
+buttonAdicionarPalavra.addEventListener("click",function(){
+  adicionarPalavra(novaPalavra.value);
+  novaPalavra.value = "";
+
+   
+});
+function adicionarPalavra(palavra){
+  palavraSecreta.push(palavra.toUpperCase());
+  palavraSecreta.forEach(function(p){
+    span.textContent = palavraSecreta.length;
+    console.log(p);
+  })
+}
+function verificarPalavra(palavra, palavraSorteada){
+  
+}
+btnLimparLista.addEventListener("click",function(){
+  palavraSecreta = [];
+  span.textContent = palavraSecreta.length;
+});
+btnPalavra.addEventListener("click", function(){
+  if(validarPalavra("teste")){
+      console("OK");
+  }
+});
+function sortearPalavra(){
+  let quantidade = palavraSecreta.length;
+  let numeroSorteado = Math.floor(Math.random() * quantidade);
+  return palavraSecreta[numeroSorteado];
+   
+}
+shapeStart();
+shape1();
+shape2();
+shape3();
+shape4();
+shape5();
+shape6();
+shape7();
+shape8();
+shape9();
+
+function validarPalavra(palavra){
+    if(palavra.length == 0){
+    
+        error.className = "error";
+        setTimeout(function() {
+          error.className = "error-invisivel";
+         }, 3000);  
+
+         return false;
+      }
+      else if(palavra.match(/[^a-z ]/g)){
+        error.textContent = "Não são permitidos caracteres especiais, números ou letras maíusculas!";
+        error.className = "error";
+        setTimeout(function(){
+          error.className = "error-invisivel";
+        },3000);
+        
+        return false;
+      }
+      else{
+        textoTitulo.textContent = "Mensagem criptografada"
+        error.className = "error-invisivel";
+        var mensagemMinuscula = codificador(result);
+        msg.value = mensagemMinuscula.replace(/[^a-z ]/gi,'');
+     return true;
+      }
+}
