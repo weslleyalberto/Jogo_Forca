e15e9e2ad44ffedf667fed3bbdd44fc2fdedc092
Weslley Alberto <weslleyalberto@gmail.com>
2022-02-07 15:46:35 -0300

    Adicionada documentação das funções no JS

--- forca.js
+++ forca.js
@@ -42,15 +42,13 @@ btnPalavra.addEventListener("click", function(){
     entradaLetra.value = '';
   }
   else{
-    erroEmpty.textContent = 'Esse campo não pode ser vazio!';
+    erroEmpty.textContent = 'Esse campo não pode ficar vazio!';
     setTimeout(function(){
       erroEmpty.textContent = '';
     },3000);
     return;
     
   }
- 
- 
 });
 btnReiniciarPartida.addEventListener("click", function(){
   document.location.reload(true);
@@ -81,59 +79,59 @@ function escolhaPalavraSecreta(){
 
 /*Comentar toda essa função , ou seja, documentar o a cada etapa da função e Retirar todos os consoles
 logs */
-function sortearPalavra(){ 
-  let letrasSplit = palavraEscolhida.split(''); 
-  for(let w=0;w<palavraEscolhida.length;w++){
-    let tracinhos = document.createElement("span");
-    tracinhos.setAttribute("ID","mostrar-tracinhos");
-    let container = document.createElement("p");
-    let innetSplit = document.createTextNode(letrasSplit[w]);
-    container.setAttribute("ID","list-container");
-    seletorEspaco.appendChild(container);
-    container.appendChild(innetSplit); 
-    containerTracinhos.appendChild(tracinhos);
+function sortearPalavra(){  // função que cria dinâmicamente com DOM , a depender do tamanho da palavra escolhida e mostra traços ou a letra , se for correta , de acordo com palavra escolhida
+  let letrasSplit = palavraEscolhida.split('');  // cria variavel letrasSplit e atribui a ela a palavra palavraEscolhida "splitada", ou seja, divite a palavra em um array de letras da palavraEscolhida;
+  for(let w=0;w<palavraEscolhida.length;w++){ // laço for para verifica percorrer de acordo com tamanho da palavraEscolhida;
+    let tracinhos = document.createElement("span"); // atribui a variável tracinho o objeto span determinado pelo DOM;
+    tracinhos.setAttribute("ID","mostrar-tracinhos"); // tracinho e atribui a ele o ID: mostrar tracinhos , que esta no CSS, para que seja mostrado os traços de acordo com tamanho da palavra escolhida.
+    let container = document.createElement("p"); //atribui a variável container a criação de um elemento p, atraves do DOM;
+    let innetSplit = document.createTextNode(letrasSplit[w]); // aqui criamos através do DOM as letrasSplit, e atribuimos a variavel declarada innetSplit ou seja da palavra escolhida, escreve a palavra escolhida e percorre ela de acordo com seu tamanho, logo mostrará a palavra escolhida;
+    container.setAttribute("ID","list-container");  // atribuimos ao container o ID list container, de define o comportamento desse container de acordo com nosso CSS
+    seletorEspaco.appendChild(container); // aqui, adicionamos o container ao seletor de espaço, ou seja como filho dele.(que esta contido nele);
+    container.appendChild(innetSplit);  // adicionaos as letras "Splitadas" no container como filho, ou seja que esta contida nele;
+    containerTracinhos.appendChild(tracinhos); // no container dos tracinhos adicionados como filho, de fato , ou tracinhos com tamanho ta palavra escolhida;
   }
   console.log(palavraEscolhida + " " + "Palavra ESCOLHIDA"); 
 }
 /*Documentar o que cada linha de código esta fazendo na função abaixo:
  */
 function validarPalavra(){
- let elementoP = document.querySelector(".container-space"); 
- let entradaUsuario = entradaLetra.value;
+ let elementoP = document.querySelector(".container-space"); // cria a variável elementoP , que é o container dos espaços
+ let entradaUsuario = entradaLetra.value; // cria variavel entradaUsuario com a letra digitada pelo usuario(o valor dela); 
  //letrasDigitadas.push(entradaUsuario);
-for(let i=0;i < palavraEscolhida.length;i++){
-   if(palavraEscolhida[i] ===entradaUsuario){
-    acertou = true;      
+for(let i=0;i < palavraEscolhida.length;i++){ // cria um laço for , de acordo com tamanho da palavra(as letras da palavra escolhida);
+   if(palavraEscolhida[i] ===entradaUsuario){ // se a palavra escolhida(a letra no momento do for), for igual a letra que o usuario digitar(ou seja veirifica se a letra que o usuario digitou está presente na palavra);
+    acertou = true;     //se a condição acima for verdadeira, irá atribuir a variavel acertou a confição de verdadeira; 
     console.log(acertos);
     console.log(palavraEscolhida.length); 
-    elementoP.children[i].style.display = 'block';
-    acertos++;
-    validarVitoria();   
+    elementoP.children[i].style.display = 'block'; //aqui ele pega o elemento filho do container-spaço  e atribui a propriedade do seu css display:block, ou seja , para que aparece as letras, respeitando a condição principal do IF
+    acertos++; //ele adiciona +1 toda vez que a condição for verdaira, logo, toda vez que a letra escolhida pelo usuario estiver condida na palavraEscolhida, ele irá adicionar +1, é necessário, pois precisamos validar se o usuario acertou todas as letras e comparar com a quantidade de letras contidas na palavra escolhida(Em outra função);
+    validarVitoria();  //chama a função de validação, que verificar o contador de acertos e o compara com a quantidade de letras condidas na palavra escolhida.(toda vez que o usuario acertar a palavra ele irá validar se a quantidade de letras acertadas está igual a quantidade de letras da palavra escolhida); 
   }
-  else if(!palavraEscolhida.includes(entradaUsuario)){
-    erros++;
-    shapes();
-    acertou = false;  
-    return;
+  else if(!palavraEscolhida.includes(entradaUsuario)){ // se não tiver (!) incluida a letra dentro da palavra escolhida (condição)
+    erros++; // Adiciona +1 no contador de erros, é necessário pois precisamos contabilizar o números de erros para determinar se o usuário perdeu o jogo;(10 no total)
+    shapes(); // cade vez que o usuario errar a letra , ele irá desenhar um pedaço da forca;
+    acertou = false;  //aqui atribuimos a variavel acertou como false, afinal, o usuario errou.
+    return; // retorna à condicional sempre que o usuario errar a letra, para que uma nova verificação ocorra;
   }      
 }
 }
-function validarVitoria(){
-  if(acertos === palavraEscolhida.length){
-    vitoria.style.display = "block"; 
+function validarVitoria(){ // valida se usuario ganhou a partida
+  if(acertos === palavraEscolhida.length){ // verifica se a quantidade de acertos é igual a quantidade de letras na palavraEscolhida(ou secreta);
+    vitoria.style.display = "block";  //se positivo, ao seletor vitoria, que corresponde a  ao span que mostra que o usuario ganhou;
   } 
 }
-function jogarNovamente(){
-  if(erros >= 10){
-    derrota.style.display = "block";    
+function jogarNovamente(){ // reinicia , ou da um reload na pagína para que o usuario possa jogar novamente
+  if(erros >= 10){ // verifica se o usuario perdeu o jogo, 
+    derrota.style.display = "block";    //se possitivo, apresenta a mensagem de jogador perdeu através da mudança do atributo css display:block do span;
   }
 }
-function validarEntradaTexto(){
-  if(entradaLetra.value.length >0 ){
-    return true;
+function validarEntradaTexto(){ // valida entrada de texto do usuario
+  if(entradaLetra.value.length >0 ){ // se usuario  digitar alguma letra
+    return true; // retorna verdadeiro
   }
-  else{
-    return false;
+  else{ // caso contrário
+    return false; //retorna falso
   }
 }
 
