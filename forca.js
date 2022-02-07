857ac99344d55813a58359a1d31cbd2ac10a02e0
Weslley Alberto <weslleyalberto@gmail.com>
2022-02-07 14:50:38 -0300

    Implementada função de converte o texto em tempo real para maiúsculo

--- forca.js
+++ forca.js
@@ -1,30 +1,49 @@
+/* Organizar todos os seletores/variáveis globais todos os seletores com query depois com byID,
+depois todas as variáveis , arrays de palavra secreta, renomear variáveis e funções para deixa-las
+mais apresentáveis.
+*/
+let erros =0;
+let acertos = 0;
+let acertou = null;
+let palavraSecreta = ["BABY"];
+let letrasDigitadas = [];
+let palavraEscolhida = [];
 let novaPalavra = document.querySelector("#input-nova-palavra");
 let letraEscolhida = document.querySelector("#palavra-text");
 let buttonAdicionarPalavra = document.querySelector("#nova-palavra");
 let btnPalavra = document.querySelector("#btnPalavra");
 let btnSortearPalavra = document.querySelector("#btn-sortear");
 let span = document.querySelector('span');
-let palavraSecreta = ["BABY"];
-let palavraEscolhida = [];
+let listContainer = document.querySelector('p');
 let btnLimparLista = document.querySelector("#btn-limpar-lista");
 let seletorEspaco = document.getElementById("container");
 let entradaLetra = document.getElementById('letra-text');
-let erros =0;
-let acertos = 0;
-let letrasDigitadas = [];
-let listContainer = document.querySelector('p');
 let vitoria = document.getElementById('vitoria');
 let derrota = document.getElementById('derrota');
 let btnReiniciarPartida = document.getElementById('btnPerdeu');
-let acertou = null;
 let containerTracinhos = document.getElementById('container-tracinhos');
+/*Organizar todos os addeventList , depois funções externas , nessa ordem */
 btnSortearPalavra.addEventListener("click", function(){ 
   escolhaPalavraSecreta();
    sortearPalavra();
 });
 buttonAdicionarPalavra.addEventListener("click",function(){
   adicionarPalavra(novaPalavra.value);
-  novaPalavra.value = "";  
+});
+btnLimparLista.addEventListener("click",function(){
+  palavraSecreta = [];
+  span.textContent = palavraSecreta.length;
+});
+btnPalavra.addEventListener("click", function(){
+  validarPalavra(); 
+  letrasDigitadas.push(entradaLetra.value);
+  entradaLetra.value = '';
+});
+btnReiniciarPartida.addEventListener("click", function(){
+  document.location.reload(true);
+});
+entradaLetra.addEventListener('input',function() {
+  entradaLetra.value =entradaLetra.value.toUpperCase();
 });
 function adicionarPalavra(palavra){
   palavraSecreta.push(palavra.toUpperCase());
@@ -32,16 +51,22 @@ function adicionarPalavra(palavra){
     span.textContent = palavraSecreta.length;
   })
 }
-btnLimparLista.addEventListener("click",function(){
-  palavraSecreta = [];
-  span.textContent = palavraSecreta.length;
-});
+
+/* Adicionar um eventListen ou trabalhar os existentes para desabilitar o botão que
+começa o jogo. Pensar em disable com setAttribute ou semelhantes*/
+/*Limpar o input depois de cada input 
+1º - Limpar o input depois de cada entrada.
+2º -  Capturar o valor da entrada e utilizar createTextNode para escreve-lá na tela.
+3º - Criar um elemento no HTML abaixo do canvas para escrever essas letras.
+OBS: Pós a letra errada ser inserida e escrita na tela, tendo como validação, se a letra está
+presenta na palavra escolhida, desabilitar essa letra para entrada futuras.
+*/
 function escolhaPalavraSecreta(){
   palavraEscolhida = palavraSecreta[Math.floor(Math.random() * palavraSecreta.length)]; 
 }
-btnPalavra.addEventListener("click", function(){
-  validarPalavra(); 
-});
+
+/*Comentar toda essa função , ou seja, documentar o a cada etapa da função e Retirar todos os consoles
+logs */
 function sortearPalavra(){ 
   let letrasSplit = palavraEscolhida.split(''); 
   for(let w=0;w<palavraEscolhida.length;w++){
@@ -54,19 +79,21 @@ function sortearPalavra(){
     container.appendChild(innetSplit); 
     containerTracinhos.appendChild(tracinhos);
   }
-  console.log(palavraEscolhida + " " + "Palavra ESC"); 
+  console.log(palavraEscolhida + " " + "Palavra ESCOLHIDA"); 
 }
+/*Documentar o que cada linha de código esta fazendo na função abaixo:
+ */
 function validarPalavra(){
  let elementoP = document.querySelector(".container-space"); 
- let entradaUsuario = entradaLetra.value.toUpperCase();
- letrasDigitadas.push(entradaUsuario);
+ let entradaUsuario = entradaLetra.value;
+ //letrasDigitadas.push(entradaUsuario);
 for(let i=0;i < palavraEscolhida.length;i++){
-   if(palavraEscolhida[i] ===entradaUsuario){    
-    acertos++;
+   if(palavraEscolhida[i] ===entradaUsuario){
+    acertou = true;      
     console.log(acertos);
-    console.log(palavraEscolhida.length);
-    acertou = true;
+    console.log(palavraEscolhida.length); 
     elementoP.children[i].style.display = 'block';
+    acertos++;
     validarVitoria();   
   }
   else if(!palavraEscolhida.includes(entradaUsuario)){
@@ -87,13 +114,6 @@ function jogarNovamente(){
     derrota.style.display = "block";    
   }
 }
-btnReiniciarPartida.addEventListener("click", function(){
-  document.location.reload(true);
-});
 
-/*
-document.addEventListener('keypress',() =>{
-  var teste = entradaLetra.key.replaceAll(/[^a-zA-Z]/g,'').toUpperCase();
-  console.log(teste);
-  entradaLetra.value = teste;
-});*/
+
+
